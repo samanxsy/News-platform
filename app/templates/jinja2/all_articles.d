bbfbcf7b0e90290c7ac64b7738fc1ab3d8f4cff1
samanxsy <samanxsy@gmail.com>
2023-03-06 15:30:16 +0100

    common-pages-edited

--- app/templates/jinja2/all_articles.d
+++ app/templates/jinja2/all_articles.d
@@ -1,7 +1,9 @@
 {% for news in articles %}
     <p class="authors">{{ news['source']['name'] }}</p>
     <h3>{{ news['title'] }}</h3>
-    <P class="description">{{ news['description'] }}</P>
+    {% if news['description'] %}
+        <P class="description">{{ news['description'] }}</P>
+    {% endif %}
     <p class="writer">By {{ news['author'] }}</p>
     <a class="links" href="{{ news['url'] }}">Read more on {{ news['source']['name'] }}</a>
     <p class="space"></p>
